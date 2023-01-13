2f084f411cdb34ecfb53e5155fc27dcdee88bf9e
Malte Becker <malte.becker91@gmail.com>
2023-01-13 18:44:07 +0100

    username is bold

--- twit2tel.py
+++ twit2tel.py
@@ -2,7 +2,7 @@ import tweepy
 import json
 import telegram
 import asyncio
-import time
+import re
 
 CONFIG_FILE = "config.json"
 async def get_ids(bot: telegram.Bot, config: dict) -> dict:
@@ -19,7 +19,7 @@ async def get_ids(bot: telegram.Bot, config: dict) -> dict:
 async def send_telegram_message(bot: telegram.Bot, config: dict, message: str):
     async with bot:
         for user_id in config["telegram_users"]:
-            await bot.send_message(text=message, chat_id=user_id)
+            await bot.send_message(text=message, chat_id=user_id, parse_mode=telegram.constants.ParseMode.HTML)
 
 def main() -> None:
     with open(CONFIG_FILE, "r") as file:
@@ -36,16 +36,25 @@ def main() -> None:
             config["last_tweets"][username] = (id, 0)
 
     for user_name, (user_id, tweet_id) in config["last_tweets"].items():
-        last_tweet = api.get_users_tweets(user_id).data[0]
+        last_tweet = api.get_users_tweets(user_id).data[5]
         if last_tweet.text.startswith("RT "):
             break
         if last_tweet.id != tweet_id:
-            asyncio.run(send_telegram_message(bot, config, f"{username}\n{last_tweet.text}"))
+            """
+            Text Layout:
+            <username> bold
+            <text from tweet> without any links
+            <link to tweet>
+            """
+            text = f"<b>{username}</b>\n{last_tweet.text}"
+            text = re.sub(r'http\S+', '', text)
+            text += f"\nhttps://twitter.com/{user_name}/status/{last_tweet.id}"
+            asyncio.run(send_telegram_message(bot, config, text))
             config["last_tweets"][username] = (user_id, last_tweet.id)
 
     data = json.dumps(config, indent=4)
-    with open(CONFIG_FILE, "w") as file:
-        file.write(data)
+    # with open(CONFIG_FILE, "w") as file:
+    #     file.write(data)
 
 
 if __name__ == "__main__":
